12fcf2a12d96e97bd4e0302772b500c235ce6b4e
Evan McCullough <webmaster@evanamccullough.com>
2014-02-09 13:53:37 -0500

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fittrack::Application.config.secret_key_base = '54a79e2c25c52b17409a1d56936d149d104aec5339b4583c52f27f40580890af8cfe744cb8504e4d617c99a83ae5cd5224858c815b5b33114be8c60bb61ed570'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fittrack::Application.config.session_store :cookie_store, key: '_fittrack_session'
