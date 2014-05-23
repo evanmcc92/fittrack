6c7ac5f72ea1d3a732140014bae2160f8f1cc3dd
Evan McCullough <webmaster@evanamccullough.com>
2014-05-22 23:05:20 -0400

    updates to uploading csv

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,8 @@
 
-require 'csv'
 
 class Exercise < ActiveRecord::Base
+
+require 'csv'
 	belongs_to :user
 	belongs_to :goal
 	belongs_to :wo_set
@@ -9,24 +10,25 @@ class Exercise < ActiveRecord::Base
 
 	def self.import(file, user_id)
     CSV.foreach(file.path, headers: true) do |row|
-      record = Exercise.find(row[0])
 
-      if record.id = row[0]
-        record.update_attributes(
-          :user_id => user_id,
-          :name => row[1],
-          :description => row[2],
-          :category => row[3]
-        )
-      else
-        record.create(
+      product_hash = row.to_hash # exclude the price field
+      exercise = Exercise.find_by(id: product_hash["id"])
+
+      if exercise.nil?
+        Exercise.create!(
           :user_id => user_id,
           :id => row[0],
           :name => row[1],
           :description => row[2],
           :category => row[3]
+          )
+      else
+        exercise.update_attributes(
+          :name => row[1],
+          :description => row[2],
+          :category => row[3]
         )
-      end
+      end # end if !product.nil?
     end
  	end
 
