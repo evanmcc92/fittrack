f8a8cb63786e635514b976d70068df43fccfbe70
Evan McCullough <webmaster@evanamccullough.com>
2014-05-24 14:06:38 -0400

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,3 @@
-if ENV["redis://rediscloud:Tze8kuOWyzHfoWnX@pub-redis-14029.us-east-1-3.1.ec2.garantiadata.com:14029
-"]
-    $redis = Redis.new(:url => ENV["redis://rediscloud:Tze8kuOWyzHfoWnX@pub-redis-14029.us-east-1-3.1.ec2.garantiadata.com:14029
-"])
-end
+
+uri = URI.parse(ENV["redis://redistogo:d0362e0f606724cd45cc16a0c1f8a218@angelfish.redistogo.com:10043/"])
+REDIS = Redis.new(:url => ENV['redis://redistogo:d0362e0f606724cd45cc16a0c1f8a218@angelfish.redistogo.com:10043/'])
