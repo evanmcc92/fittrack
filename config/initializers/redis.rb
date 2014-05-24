ebb301758243aae24a155bb271d754674bbfb4eb
Evan McCullough <webmaster@evanamccullough.com>
2014-05-24 19:56:25 -0400

    updates

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/0/fit-track" )
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379/" )
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
