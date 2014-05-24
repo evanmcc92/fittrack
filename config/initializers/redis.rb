7cc08c203af16f07bf6e9cb606831cc18e55eb9a
Evan McCullough <webmaster@evanamccullough.com>
2014-05-24 14:01:04 -0400

    updates

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+if ENV["redis://rediscloud:Tze8kuOWyzHfoWnX@pub-redis-14029.us-east-1-3.1.ec2.garantiadata.com:14029
+"]
+    $redis = Redis.new(:url => ENV["redis://rediscloud:Tze8kuOWyzHfoWnX@pub-redis-14029.us-east-1-3.1.ec2.garantiadata.com:14029
+"])
 end
