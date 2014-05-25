adbf440535748724977b3a6e3618751c5bc4d99a
Evan McCullough <webmaster@evanamccullough.com>
2014-05-24 20:46:19 -0400

    updates

--- config/initializers/resque.rb
+++
@@ -1,5 +0,0 @@
-ENV["REDISTOGO_URL"] ||= 'redis://redistogo:d0362e0f606724cd45cc16a0c1f8a218@angelfish.redistogo.com:10043/'
-
-
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
