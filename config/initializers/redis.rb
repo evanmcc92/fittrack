f1060fef6ea9c418fc568515a463686b8db37060
Evan McCullough <webmaster@evanamccullough.com>
2014-05-24 20:09:16 -0400

    updates

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379/" )
+uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
