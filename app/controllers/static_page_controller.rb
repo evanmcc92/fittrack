c4d0825bcae997a4eec7025d16c1ac057ee6d897
Evan McCullough <webmaster@evanamccullough.com>
2014-06-15 18:06:38 -0400

    updates to edit user page

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -3,7 +3,6 @@ class StaticPageController < ApplicationController
   def index
     if signed_in?
 		@feed_items = current_user.feed.paginate(:page => params[:page])
-		@workout = Workout.find_by(@feed_items.model_id)
 		@post = current_user.posts.build
 		@user = User.find_by(params[:voter_id])
 	end
